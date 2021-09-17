49b383e3041162549ed6f794651f936c1c742c2b
jithinbabu296 <71069273+jithinbabu296@users.noreply.github.com>
2021-09-17 09:54:37 +0530

    add authour name

--- product_route_selection/__manifest__.py
+++ product_route_selection/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "14.0.1",
     "category": "Inventory",
     "summary": "Selection of routes in product",
-    "author": "SGG",
+    "author": "SGG-jithin",
     "depends": ["stock",'purchase'],
     "data": [
         # "security/ir.model.access.csv",
